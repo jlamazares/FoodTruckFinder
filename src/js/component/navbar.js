253b93d6763dd72ffacab9605edcf923250bce50
jlamazares <jlamazares01@gmail.com>
2019-10-08 00:10:30 +0000

    adding component names

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,11 +6,11 @@ export class Navbar extends React.Component {
 		return (
 			<nav className="navbar navbar-light bg-light mb-3">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">Food Truck Finder</span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-primary">Home</button>
 					</Link>
 				</div>
 			</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,18 +1,12 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
-
+import { ControlledCarousel } from "../component/carousel";
 export class Home extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
-				</a>
+				<h1>Testing carousel</h1>
+				<ControlledCarousel />
 			</div>
 		);
 	}
